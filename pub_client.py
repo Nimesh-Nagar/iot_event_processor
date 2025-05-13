36732f53a477e5a07a61b8756c5d0c0cd755fb39
Nimesh-Nagar <n.nimesh02@gmail.com>
2025-05-13 21:49:38 +0530

    mqtt publisher core

---
+++ pub_client.py
@@ -0,0 +1,60 @@
+import asyncio
+import json
+from gmqtt import Client as MQTTClient
+from datetime import datetime
+
+MQTT_HOST = "localhost"  #docker mqtt-broker ip
+PORT = 1883
+PUB_TOPIC = "device/events"
+
+STOP = asyncio.Event()
+
+
+def on_connect(client, flags, rc, properties):
+    print("Connected to MQTT broker")
+
+def on_disconnect(client, packet, exc=None):
+    print("Disconnected")
+
+async def publish_message():
+    client = MQTTClient("publisher_client")
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+
+    await client.connect(MQTT_HOST, PORT)
+
+    message1 = {
+        "device_id": "1001",
+        "sensor_type": "humidity",
+        "sensor_value": 82.2,
+        "timestamp": datetime.now().isoformat()
+        # "timestamp": "12:13:55"
+    }
+
+    message2 = {
+        "device_id": "1002",
+        "sensor_type": "temperature",
+        "sensor_value": 25.6,
+        "timestamp": datetime.now().isoformat()
+        # "timestamp": "12:13:55"
+    }
+
+    json_payload = json.dumps(message2) #change message number accordingly
+
+    client.publish(PUB_TOPIC, json_payload, qos=1)
+    print(f"Published: {json_payload}")
+
+    await asyncio.sleep(2)
+    await client.disconnect()
+    STOP.set()
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    try:
+        loop.run_until_complete(publish_message())
+    finally:
+        loop.close()
+        print("[INFO] Publisher disconnected")
